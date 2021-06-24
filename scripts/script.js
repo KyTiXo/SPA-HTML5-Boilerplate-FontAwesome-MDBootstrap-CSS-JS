e342a2370f87c4724de32de4d2aec3dbb1520a9c
Kyle <zkoolkyle@gmail.com>
2021-06-24 12:38:07 -0400

    First Commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // do stuff on launch
+});
+
+function downloadAction(passedVar) {
+    // action when download button is clicked
+    alert(passedVar);
+}
